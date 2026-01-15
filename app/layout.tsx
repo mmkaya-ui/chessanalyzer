d37599d9b6e3338e2d8c05bc969ad984b0fa14dd
mmkaya-ui <mertmercankaya@gmail.com>
2026-01-15 17:32:03 +0100

    debug: add global error logger and disable background

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <div id="global-error-log" style={{ position: 'fixed', top: 0, left: 0, width: '100%', background: 'red', color: 'white', zIndex: 99999, display: 'none', padding: '10px' }}></div>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.onerror = function(msg, url, line) {
+            const el = document.getElementById('global-error-log');
+            if(el) {
+              el.style.display = 'block';
+              el.innerHTML += "<div>Error: " + msg + " at line " + line + "</div>";
+            }
+          };
+        `}} />
         {children}
       </body>
     </html>
